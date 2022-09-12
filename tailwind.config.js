fc91389f35ac4b62b79173fa6c2fcc6ccdbe6622
PranoySarker <pranoysarker01@gmail.com>
2022-09-12 23:36:38 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        doctheme: {
+          primary: "#2D89FF",
+          secondary: "#642DFF",
+          paragraph1: "#575757",
+          textb: "#1E1E1E",
+          "base-100": "#F7F7F7",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
